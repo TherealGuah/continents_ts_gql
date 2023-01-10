314b2e25818a7866f04edc14e1c19a50ad1c4f58
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-10 17:11:51 +0000

    base Country component created

--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -1,10 +1,16 @@
-
+import React, {useEffect, useState} from "react";
+import { Link } from 'react-router-dom';
 function Country(): JSX.Element {
-
-  return  (
+    useEffect(() => {
+        // TODO: Mount the selected country by code
+    },[])
+    return  (
       <>
+          <Link to={'/'}>
+              <button>Back to Continents</button>
+          </Link>
       </>
-  );
+    );
 }
 
 export default Country;
