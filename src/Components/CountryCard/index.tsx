9d39fda3f355e24c7383df72c131bcbb761adcc9
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-10 17:05:34 +0000

    added link to countries/code

--- src/Components/CountryCard/index.tsx
+++ src/Components/CountryCard/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface countryJson {
     country: {
+        code: string
         name: string,
         capital: string,
         currency: string,
@@ -13,15 +15,17 @@ interface countryJson {
 
 const CountryCard = (props: countryJson): JSX.Element => {
 
-    const {name, capital, currency, languages} = props.country;
+    const { code, name, capital, currency, languages} = props.country;
 
     return (
-        <div className="p-8 m-4 shadow-xl rounded-lg hover:border-t-2 hover:border-l-2">
-            <h3 className="font-bold">{name}</h3>
-            <p>{languages[0] ? languages[0].name : ''}</p>
-            <p>{capital ? capital : ''}</p>
-            <p>{currency ? currency : ''}</p>
-        </div>
+        <Link to={`/countries/${code}`}>
+            <div className="p-8 m-4 shadow-xl rounded-lg hover:border-t-2 hover:border-l-2">
+                <h3 className="font-bold">{name}</h3>
+                <p>{languages[0] ? languages[0].name : ''}</p>
+                <p>{capital ? capital : ''}</p>
+                <p>{currency ? currency : ''}</p>
+            </div>
+        </Link>
     );
 };
 
