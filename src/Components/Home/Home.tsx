71b6a0809099280bf990ef4c46fccc1f4579ae25
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-10 16:58:15 +0000

    Element to load on Mount

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,56 @@
+import Continent from "../Continent";
+import CountryCard from "../CountryCard";
+import React, {useEffect, useState} from "react";
+import {fetchContinents, fetchCountriesByContinent} from "../../API/apiRequests";
+
+function Home(): JSX.Element {
+
+    const [continents, setContinents] = useState([]);
+    const [countries, setCountries] = useState([]);
+    const [continentCode, setContinentCode] = useState('');
+
+    useEffect(() => {
+        fetchContinents()
+            .then(response => setContinents(response.data.continents))
+    },[]);
+
+
+    useEffect(() => {
+        fetchCountriesByContinent(continentCode)
+            .then( response => {
+                setCountries(response.data.continent.countries);
+            })
+    },[continentCode]);
+
+    const handleContinentChange = (ev: any): void => {
+        ev.preventDefault();
+        setContinentCode(ev.target.value);
+    };
+
+    return (
+        <div className="App">
+
+            <div className="border-b-2 mx-8">
+                <h1 className="text-4xl font-black py-8">Countries by Continent</h1>
+            </div>
+
+            <div className="flex flex-row mx-8 py-8">
+                <div className="w-1/5 mr-16" >
+                    <ul className="border-b-2  flex flex-col justify-between ">
+                        {
+                            continents.map( (continent, index) => <Continent key={index} continent={continent} handleContinentChange={handleContinentChange}/>)
+                        }
+                    </ul>
+                </div>
+
+                <div className="border-radius-4 border-4 rounded w-4/5 flex flex-wrap justify-between">
+                    {
+                        countries.map( (country, index) => <CountryCard key={index} country={country} />)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
