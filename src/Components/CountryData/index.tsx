289c22a5c79e317bc44475efe7e02566f307477e
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-02-22 22:08:26 +0000

    Add interface for Props and referenced all props from country

--- src/Components/CountryData/index.tsx
+++ src/Components/CountryData/index.tsx
@@ -1,6 +1,25 @@
-//TODO: passar tudo po component acima...
+interface Country {
+    country: {
+        code: string,
+        name: string,
+        native: string,
+        phone: string,
+        continent: {
+            name: string
+        },
+        capital: string,
+        currency: string,
+        languages: [
+            {
+                name: string
+            }
+        ],
+        emoji: string
+    }
+}
 
-function CountryData(props: any): JSX.Element {
+function CountryData(props: Country): JSX.Element {
+
     const {
         code,
         name,
@@ -12,19 +31,23 @@ function CountryData(props: any): JSX.Element {
         languages,
         emoji
     } = props.country;
+
     return (
         <>
-            <h2>Country Name: {name}</h2>
+            <p>Name: {name}</p>
             <p>International Code (AF): {code}</p>
             <p>Native Name: {native}</p>
             <p>Telephone Code: +{phone}</p>
-            {/*<p>{continent}</p>*/}
+            <p>Continent: {continent ? continent.name : ''}</p>
             <p>Capital: {capital}</p>
             <p>Currency: {currency}</p>
-
-            <p>Flag emoji: {emoji}</p>
+            <p>Languages:</p>
+            <ul>
+                {languages ? languages.map( (lang, index) => <li key={index}>{lang.name}</li>) : ''}
+            </ul>
+            <p>Flag: {emoji}</p>
         </>
     );
-}
+};
 
 export default CountryData;
